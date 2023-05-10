2459d2a14d8c8585224c3d4d40a4e9bf154397a6
zzzzer <zzzzer91@gmail.com>
2023-05-10 22:04:39 +0800

    feat: update jsonx pkg

--- jsonx/jsonx.go
+++ jsonx/jsonx.go
@@ -1,13 +1,48 @@
 package jsonx
 
-import "github.com/bytedance/sonic"
+import (
+	"github.com/bytedance/sonic"
+	"github.com/pkg/errors"
+)
 
-func Marshal(obj any) []byte {
+func Marshal(obj any) ([]byte, error) {
+	res, err := sonic.ConfigDefault.Marshal(obj)
+	if err != nil {
+		return nil, errors.Wrap(err, "Marshal error")
+	}
+	return res, nil
+}
+
+func MarshalToString(obj any) (string, error) {
+	res, err := sonic.ConfigDefault.MarshalToString(obj)
+	if err != nil {
+		return "", errors.Wrap(err, "MarshalToString error")
+	}
+	return res, nil
+}
+
+func Unmarshal(data []byte, obj any) error {
+	err := sonic.ConfigDefault.Unmarshal(data, obj)
+	if err != nil {
+		return errors.Wrap(err, "Unmarshal error")
+	}
+	return nil
+}
+
+func UnmarshalFromString(data string, obj any) error {
+	err := sonic.ConfigDefault.UnmarshalFromString(data, obj)
+	if err != nil {
+		return errors.Wrap(err, "UnmarshalFromString error")
+	}
+	return nil
+}
+
+func Marshal2(obj any) []byte {
 	res, _ := sonic.ConfigDefault.Marshal(obj)
 	return res
 }
 
-func MarshalToString(obj any) string {
+func MarshalToString2(obj any) string {
 	res, _ := sonic.ConfigDefault.MarshalToString(obj)
 	return res
 }
